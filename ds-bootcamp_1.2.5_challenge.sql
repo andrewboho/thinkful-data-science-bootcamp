8c0837f0ec7819012934d205a53a2b734b473017
andrewboho <aab_01_public@icloud.com>
2018-05-15 20:41:37 -0500

    commit

---
+++ ds-bootcamp_1.2.5_challenge.sql
@@ -0,0 +1,32 @@
+-- ds-bootcamp_1.2.5_challenge
+-- Tasmania, Tasmania, Australia
+
+-- What's the most expensive listing? What else can you tell me about the listing?
+SELECT *
+FROM
+    listings
+ORDER BY listings.price DESC
+LIMIT 3
+-- There were three tied for 1500.
+
+
+
+-- What neighborhoods seem to be the most popular?
+SELECT
+    listings.neighbourhood,
+    SUM(listings.number_of_reviews) total_reviews
+FROM
+    listings
+GROUP BY listings.neighbourhood
+ORDER BY total_reviews DESC
+-- Hobert by a wide margin with Launceston in second place
+
+-- What time of year is the cheapest time to go to your city? What about the busiest?
+SELECT
+    strftime('%m', DATETIME(reviews.date)) month,
+    COUNT(*) count_reviews
+FROM
+    reviews
+GROUP BY month
+ORDER BY count_reviews DESC
+
