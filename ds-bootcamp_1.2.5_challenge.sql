88fcaa6a34cfbc16b8065e311c25d9e20b9049f1
andrewboho <aab_01_public@icloud.com>
2018-05-16 17:09:29 -0500

    commit

--- ds-bootcamp_1.2.5_challenge.sql
+++ ds-bootcamp_1.2.5_challenge.sql
@@ -9,8 +9,6 @@ ORDER BY listings.price DESC
 LIMIT 3
 -- There were three tied for 1500.
 
-
-
 -- What neighborhoods seem to be the most popular?
 SELECT
     listings.neighbourhood,
@@ -22,6 +20,20 @@ ORDER BY total_reviews DESC
 -- Hobert by a wide margin with Launceston in second place
 
 -- What time of year is the cheapest time to go to your city? What about the busiest?
+
+SELECT
+    strftime('%m', DATETIME(reviews.date)) month,
+    SUM(listings.price) total_monthly_price
+FROM
+    listings
+JOIN
+    reviews
+ON
+    listings.id = reviews.listing_id
+GROUP BY month
+ORDER BY total_monthly_price ASC
+-- Cheapest months would be August and May
+
 SELECT
     strftime('%m', DATETIME(reviews.date)) month,
     COUNT(*) count_reviews
@@ -29,4 +41,5 @@ FROM
     reviews
 GROUP BY month
 ORDER BY count_reviews DESC
+-- Busiest time of year would be December/January
 
